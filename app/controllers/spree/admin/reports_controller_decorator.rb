034fcc9fcd810d3a9ca33c3c46e4fd4f3124d427
Maria Jose <sechix@gmail.com>
2018-08-30 12:46:01 +0200

    update admin routes

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -1,5 +1,9 @@
 Spree::Admin::ReportsController.class_eval do
-  add_available_report! :stock_requests
+  before_action :add_stock_requests, only: [:index]
+  
+  def add_stock_requests
+    Spree::Admin::ReportsController.add_available_report! :stock_requests
+  end  
 
   def stock_requests
     @search = Spree::StockRequest.ransack(params[:q])
