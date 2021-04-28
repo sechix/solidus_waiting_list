30a24800dd44036494296f5c4b8da662a3f9b3ab
Maria Jose <sechix@gmail.com>
2021-04-28 17:28:34 +0200

    Update permitted_attributes_decorator.rb

--- app/models/spree/permitted_attributes_decorator.rb
+++ app/models/spree/permitted_attributes_decorator.rb
@@ -1,13 +1,9 @@
-Spree::PermittedAttributes.class_eval do
-  @@stock_request_attributes = [:email, :variant_id]
-end
-
 module Spree
   module PermittedAttributes
     class << self
       mattr_reader :stock_request_attributes
 
-      ATTRIBUTES << :stock_request_attributes
+      ATTRIBUTES << [:email, :variant_id]
     end
   end
 end
