0a42dc3b42010b08cd4198cf36786bc6f2f38a36
Maria Jose <sechix@gmail.com>
2021-07-15 11:50:25 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Spree::Core::Engine.routes.prepend do
   resources :stock_requests
 
   namespace :admin do
-    resources :reports, :only => [:index, :show] do
+    resources :reports, :only => [:index] do
       get 'stock_requests', :on => :collection
     end
   end
